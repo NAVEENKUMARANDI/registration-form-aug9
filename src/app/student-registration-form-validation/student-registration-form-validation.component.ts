98bce2ad63231c59bcb7b99bfe51abda05ead093
NaveenKumar <naveenkumar.andi@gmail.com>
2019-08-08 16:18:18 +0530

    Second Commit

--- src/app/student-registration-form-validation/student-registration-form-validation.component.ts
+++ src/app/student-registration-form-validation/student-registration-form-validation.component.ts
@@ -12,6 +12,10 @@ export class StudentRegistrationFormValidationComponent implements OnInit {
   studentProfile = this.fb.group({
     firstName: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
     lastName: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+    email: ['', [Validators.required, Validators.maxLength(30)]],
+    mobileNumber: ['', [Validators.required, Validators.maxLength(10)]],
+    gender: ['',[Validators.required]],
+    address: ['', [Validators.required]]
   });
 
   constructor(private fb: FormBuilder) { }
@@ -27,4 +31,19 @@ export class StudentRegistrationFormValidationComponent implements OnInit {
     return this.studentProfile.get('lastName');
   }
 
+  get email() {
+    return this.studentProfile.get('email');
+  }
+
+  get mobileNumber() {
+    return this.studentProfile.get('mobileNumber');
+  }
+  
+  get gender() {
+    return this.studentProfile.get('gender');
+  }
+
+  get password() {
+    return this.studentProfile.get('address');
+  }
 }
