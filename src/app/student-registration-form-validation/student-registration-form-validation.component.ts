f94f8db1fd30d3e6b979cd1e17faf6ac0fa4de5a
NaveenKumar <naveenkumar.andi@gmail.com>
2019-08-11 22:32:13 +0530

    Submit button added in html file

--- src/app/student-registration-form-validation/student-registration-form-validation.component.ts
+++ src/app/student-registration-form-validation/student-registration-form-validation.component.ts
@@ -22,7 +22,12 @@ export class StudentRegistrationFormValidationComponent implements OnInit {
     city: ['', [Validators.required]],
     pinCode: ['', [Validators.required, Validators.minLength(6)]],
     state: ['', [Validators.required]],
-    country: ['', [Validators.required]]
+    country: ['', [Validators.required]],
+    drawing: ['', [Validators.required]],
+    singing: ['', [Validators.required]],
+    dancing: ['', [Validators.required]],
+    others: ['', [Validators.required]],
+    Other_Hobby: ['',[Validators.required, Validators.maxLength(15)]]
   });
 
   constructor(private fb: FormBuilder) { }
@@ -66,6 +71,21 @@ export class StudentRegistrationFormValidationComponent implements OnInit {
     return this.studentProfile.get('country');
   }
 
+  get drawing() {
+    return this.studentProfile.get('drawing');
+  }
+  get singing() {
+    return this.studentProfile.get('singing');
+  }
+  get dancing() {
+    return this.studentProfile.get('dancing');
+  }
+  get others() {
+    return this.studentProfile.get('others');
+  }
+  get Other_Hobby() {
+    return this.studentProfile.get('Other_Hobby');
+  }
   onSubmit() {
     console.warn(this.studentProfile.value);
   }
